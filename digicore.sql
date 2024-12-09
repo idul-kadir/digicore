ef67e721a05d1da4c937a4dd3312e0de3724b6d0
idulkadir <idulkadir10@gmail.com>
2024-12-09 08:26:57 +0800

    lanjut dikampus. insya Allah

--- digicore.sql
+++ digicore.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Waktu pembuatan: 06 Des 2024 pada 00.46
+-- Waktu pembuatan: 09 Des 2024 pada 01.26
 -- Versi server: 10.4.32-MariaDB-log
 -- Versi PHP: 8.2.12
 
@@ -41,24 +41,9 @@ CREATE TABLE `connector` (
 --
 
 INSERT INTO `connector` (`id`, `jenis`, `ip`, `catatan`, `config`, `status`) VALUES
-(1733402201, 'ANY', '15.50.22.150', '2571', '', 'tidak aktif'),
-(1733402248, 'Open VPN', '13.26.14.157', '8264', '', 'tidak aktif'),
-(1733402268, 'Open VPN', '52.66.21.3', '7594', '', 'tidak aktif'),
-(1733402290, 'ANY', '58.212.212.212', '3318', '', 'tidak aktif'),
-(1733402354, 'ANY', '159.20.20.3', '4687', '', 'tidak aktif'),
-(1733402557, 'ANY', '10.64.66.101', '0115', '', 'tidak aktif'),
-(1733402575, 'ANY', '10.64.66.107', '8658', '', 'tidak aktif'),
-(1733402590, 'ANY', '190.64.66.101', '6621', '', 'tidak aktif'),
-(1733402605, 'ANY', '10.64.67.101', '7881', '', 'tidak aktif'),
-(1733402630, 'ANY', '109.64.66.101', '2560', '', 'tidak aktif'),
-(1733402647, 'ANY', '90.64.66.101', '7617', '', 'tidak aktif'),
-(1733402666, 'ANY', '10.64.66.108', '3166', '', 'tidak aktif'),
-(1733402685, 'Open VPN', '10.64.36.101', '8552', '', 'tidak aktif'),
-(1733402708, 'Open VPN', '10.64.86.51', '9447', '', 'tidak aktif'),
-(1733402749, 'Open VPN', '10.24.66.101', '0137', '', 'tidak aktif'),
 (1733406230, 'Wireguard', '185.22.25.2', '', 'assets/konektor/DigiCore-Wireguard-1733406230.conf', 'aktif'),
-(1733406248, 'Wireguard', '185.22.25.3', '', 'assets/konektor/DigiCore-Wireguard-1733406248.conf', 'aktif'),
-(1733406266, 'Wireguard', '185.22.25.4', '', 'assets/konektor/DigiCore-Wireguard-1733406266.conf', 'aktif'),
+(1733406248, 'Wireguard', '185.22.25.3', '', 'assets/konektor/DigiCore-Wireguard-1733406248.conf', 'tidak aktif'),
+(1733406266, 'Wireguard', '185.22.25.4', '', 'assets/konektor/DigiCore-Wireguard-1733406266.conf', 'tidak aktif'),
 (1733406284, 'Wireguard', '185.22.25.5', '', 'assets/konektor/DigiCore-Wireguard-1733406284.conf', 'tidak aktif'),
 (1733406300, 'Wireguard', '185.22.25.6', '', 'assets/konektor/DigiCore-Wireguard-1733406300.conf', 'tidak aktif'),
 (1733406315, 'Wireguard', '185.22.25.7', '', 'assets/konektor/DigiCore-Wireguard-1733406315.conf', 'tidak aktif'),
@@ -70,7 +55,14 @@ INSERT INTO `connector` (`id`, `jenis`, `ip`, `catatan`, `config`, `status`) VAL
 (1733406458, 'Wireguard', '185.22.25.13', '', 'assets/konektor/DigiCore-Wireguard-1733406458.conf', 'tidak aktif'),
 (1733406477, 'Wireguard', '185.22.25.14', '', 'assets/konektor/DigiCore-Wireguard-1733406477.conf', 'tidak aktif'),
 (1733406495, 'Wireguard', '185.22.25.15', '', 'assets/konektor/DigiCore-Wireguard-1733406495.conf', 'tidak aktif'),
-(1733406513, 'Wireguard', '185.22.25.16', '', 'assets/konektor/DigiCore-Wireguard-1733406513.conf', 'tidak aktif');
+(1733406513, 'Wireguard', '185.22.25.16', '', 'assets/konektor/DigiCore-Wireguard-1733406513.conf', 'tidak aktif'),
+(1733521774, 'ANY', '128.44.56.2', '6694', '', 'aktif'),
+(1733522024, 'OVPN', '128.44.56.3', '3594', '', 'tidak aktif'),
+(1733522042, 'OVPN', '128.44.56.4', '8159', '', 'tidak aktif'),
+(1733522053, 'ANY', '128.44.56.5', '5142', '', 'tidak aktif'),
+(1733522065, 'ANY', '128.44.56.6', '6252', '', 'tidak aktif'),
+(1733522079, 'ANY', '128.44.56.7', '8082', '', 'tidak aktif'),
+(1733522092, 'ANY', '128.44.56.8', '9691', '', 'tidak aktif');
 
 -- --------------------------------------------------------
 
@@ -88,6 +80,13 @@ CREATE TABLE `l_vpn` (
   `perpanjang` enum('ya','tidak') NOT NULL,
   `status` set('aktif','tidak aktif','expired') NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Dumping data untuk tabel `l_vpn`
+--
+
+INSERT INTO `l_vpn` (`id`, `kode_produk`, `id_user`, `konektor1`, `konektor2`, `tgl_expired`, `perpanjang`, `status`) VALUES
+(1733526226, 'TUNNEL 3', '1732487217', '1733406230', '1733521774', '2025-01-06', 'ya', 'aktif');
 
 -- --------------------------------------------------------
 
@@ -145,10 +144,33 @@ INSERT INTO `produk` (`kode`, `nama`, `kategori`, `harga`, `sk_terima`, `sk_tola
 ('SEMPURNA', 'SEMPURNA', 'Whatsapp', 120000, '[\"Berlaku 1 bulan\",\"Unlimited pesan\",\"Nomor disediakan oleh DigiCore\",\"Didukung Rest API\",\"Monitoring pesan\",\"Nomor pengirim tetap\",\"Perpanjang otomatis\",\"Dukungan tim teknis penuh\",\"Nomor dedicated\",\"Webhook\"]', '[\"\"]', 30, 'aktif'),
 ('SPONSORSHIP', 'SPONSORSHIP', 'Whatsapp', 150000, '[\"Berlaku sesuai kesepakatan\",\"Unlimited pesan\",\"Nomor disediakan oleh DigiCore\",\"Didukung Rest API\",\"Monitoring pesan\",\"Nomor pengirim tetap\",\"Perpanjang otomatis\",\"Dukungan tim teknis penuh\",\"Nomor dedicated\",\"Webhook\"]', '[\"\"]', 30, 'aktif'),
 ('TUNNEL 1', 'TUNNEL 1', 'VPN Tunnel', 5000, '[\"1 Port\",\"Masa aktif 1 bulan\",\"Unlimited Bandwith\",\"Unlimited Kuota\",\"VPN Wireguard\",\" VPN Openvpn\",\"Server USA\",\"Protocol TCP\"]', '[\"\"]', 30, 'aktif'),
-('TUNNEL 3', 'TUNNEL 3', 'VPN Tunnel', 10000, '[\"3 Port\",\" Masa aktif 1 bulan\",\"Unlimited Bandwith\",\" Unlimited Kuota\",\"VPN Wireguard\",\" VPN Openvpn\",\"Server USA\",\" Protokol TCP\"]', '[\"\"]', 30, 'aktif'),
+('TUNNEL 3', 'TUNNEL 3', 'VPN Tunnel', 7000, '[\"3 Port\",\" Masa aktif 1 bulan\",\"Unlimited Bandwith\",\" Unlimited Kuota\",\"VPN Wireguard\",\" VPN Openvpn\",\"Server USA\",\" Protokol TCP\"]', '[\"\"]', 30, 'aktif'),
 ('TUNNEL 80', 'TUNNEL WEB SERVER', 'VPN Tunnel', 10000, '[\"Port 80\",\"Masa aktif 1 bulan\",\"Bandwith 5Mbps Dedicated\",\"Maksimal 2 domain\",\" GRATIS SSL\",\"Gratis 2 sub-domain DigiCore\",\"Dukungan Tim Teknis\"]', '[\"\"]', 30, 'aktif'),
 ('USA 1', 'USA', 'VPS', 50000, '[\"CPU 1 core\",\"RAM 512 MB\",\"Storage 15 GB\",\"Bandwith 0.5 TB\",\"Location USA\",\"Harga bulanan\"]', '[\"\"]', 30, 'aktif'),
 ('USA 2', 'USA 2', 'VPS', 69500, '[\"CPU 1 Core\",\"RAM 1 GB\",\" Storage 25 GB\",\"Bandwith 1TB\",\"Location USA\"]', '[\"\"]', 30, 'aktif');
+
+-- --------------------------------------------------------
+
+--
+-- Struktur dari tabel `tunnel`
+--
+
+CREATE TABLE `tunnel` (
+  `id` int(11) NOT NULL,
+  `id_vpn` varchar(20) NOT NULL,
+  `ip` varchar(50) NOT NULL,
+  `src_port` varchar(10) NOT NULL,
+  `dst_port` varchar(10) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Dumping data untuk tabel `tunnel`
+--
+
+INSERT INTO `tunnel` (`id`, `id_vpn`, `ip`, `src_port`, `dst_port`) VALUES
+(4, '1733526226', '128.44.56.2', '8411', '21'),
+(5, '1733526226', '128.44.56.2', '7492', '25'),
+(6, '1733526226', '128.44.56.2', '6233', '22');
 
 -- --------------------------------------------------------
 
@@ -170,7 +192,7 @@ CREATE TABLE `user` (
 --
 
 INSERT INTO `user` (`id`, `nama`, `wa`, `password`, `saldo`, `aktifitas`) VALUES
-(1732487217, 'Ridwan Kadir', '089669106718', '865a4f84143b1e26f0289fae728a62e9', 20000, '');
+(1732487217, 'Ridwan Kadir', '089669106718', '865a4f84143b1e26f0289fae728a62e9', 33000, '');
 
 --
 -- Indexes for dumped tables
@@ -201,10 +223,26 @@ ALTER TABLE `produk`
   ADD PRIMARY KEY (`kode`);
 
 --
+-- Indeks untuk tabel `tunnel`
+--
+ALTER TABLE `tunnel`
+  ADD PRIMARY KEY (`id`);
+
+--
 -- Indeks untuk tabel `user`
 --
 ALTER TABLE `user`
   ADD PRIMARY KEY (`id`);
+
+--
+-- AUTO_INCREMENT untuk tabel yang dibuang
+--
+
+--
+-- AUTO_INCREMENT untuk tabel `tunnel`
+--
+ALTER TABLE `tunnel`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=7;
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
