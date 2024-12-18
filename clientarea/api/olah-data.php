7b5e4cf382c7a726549e1ea7951d9cb72b9a66b3
idul-kadir <idulkadir10@gmail.com>
2024-12-18 16:36:31 +0800

    lanjut dirumah

--- clientarea/api/olah-data.php
+++ clientarea/api/olah-data.php
@@ -6,17 +6,29 @@ header("Content-Type: application/json");
 
 $keterangan = end(explode('/',$_SERVER['REDIRECT_URL']));
 $jenis_request = $_SERVER['REQUEST_METHOD'];
+$header = getallheaders();
+$data = json_decode(file_get_contents("php://input"), true);
 
 switch($keterangan){
   case 'send-message':
     if($jenis_request == 'POST'){
+      if(isset($header['Apikey'])){
+        $apikey = bersihkan($header['Apikey']);
+        $cek_api = query("SELECT * FROM `l_whatsapp` WHERE apikey='$apikey' AND `status`='aktif' ");
+        if(mysqli_num_rows($cek_api)>0){
 
+        }else{
+          $result = ['kode' => 501, "keterangan" => "Apikey tidak valid atau telah kadaluarsa"];
+        }
+      }else{
+        $result = ['kode' => 501, "keterangan" => "Apikey tidak boleh kosong"];
+      }
     }else{
-      $result = ['kode' => 500, "keterangan" => "Request harus POST"];
+      $result = ['kode' => 501, "keterangan" => "Request harus POST"];
     }
     break;
   
-  default: $result = ['kode' => 404, "keterangan" => "Tujuan tidak diketahui"];
+  default: $result = ['kode' => 404, "keterangan" => "Endpoint tidak diketahui"];
 }
 
 echo json_encode($result);
