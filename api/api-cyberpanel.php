aaadbd9bb8596a20db11bfe16e221a4cedce5afa
idulkadir <idulkadir10@gmail.com>
2025-01-30 08:37:44 +0800

    api cyberpanel

--- api/api-cyberpanel.php
+++ api/api-cyberpanel.php
@@ -1,10 +1,20 @@
 <?php
+$list_ip = [''];
 
-if(isset($_GET['aksi'])){
-  $list = getallheaders();
-  $file = fopen("header-cyberpanel.json","w+");
-  fwrite($file, json_encode($list));
-  fclose($file);
+$url = $_SERVER['REQUEST_URI'];
+$data = json_decode(file_get_contents("php://input"));
+
+if(isset($data)){
+  $pecah = explode('/',$url);
+  $keterangan = end($pecah);
+
+  switch ($keterangan){
+    case 'buat-website':
+      $result = ["adminUser" => $_SERVER['USER_CP'], "adminPass" => $_SERVER['PASS_CP'], "domainName" => $data['domainName'], "ownerEmail" => $data['ownerEmail'], "packageName" => $data['packageName'], "websiteOwner" => $data['websiteOwner'], "ownerPassword" => $data['ownerPassword']];
+    break;
+    
+    default: $result = ["status" => "Aksi tidak terdefinisi"];
+  }
 
   echo json_encode(["result" => "okeh"]);
 }
